69bb424c0a8f003f2e6b32c2c292813758e00ded
Peter <PeterRussellCraig@gmail.com>
2024-03-27 16:02:29 -0500

    updated dependencies and added packaging

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   description = "A simple Go package";
 
   # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
-
-  outputs = { self, nixpkgs }:
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, gomod2nix }:
     let
 
       # to work with older version of flakes
@@ -20,10 +25,28 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor = forAllSystems (system: import nixpkgs { 
+        inherit system; 
+        overlays = [ gomod2nix.overlays.default ];
+      });
 
     in
     {
+      packages = forAllSystems (system: 
+      let
+        pkgs = nixpkgsFor.${system};
+      in 
+      {
+        default = pkgs.buildGoApplication {
+          pname = "aggreRSS";
+          version = "0.0.1";
+          pwd = ./.;
+          src = ./.;
+          modules = ./gomod2nix.toml;
+          go = pkgs.go;
+        };
+        
+      });
       # Add dependencies that are only needed for development
       devShells = forAllSystems (system:
         let 
@@ -36,6 +59,7 @@
               gopls 
               gotools 
               go-tools
+              gomod2nix.packages.${system}.default
               postgresql
               pgadmin4
               sqlc
